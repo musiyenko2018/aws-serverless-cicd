3a3be6267fa9dc98ff082cfa927ad25d0905cb85
4c27693b <4c27693b@cloud-mentor.local>
2026-09-19 16:58:09 +0000

    ci: verification 20260919165809

--- lambdas/cmtr_4c27693b_lambda_createOrder/handler.py
+++ lambdas/cmtr_4c27693b_lambda_createOrder/handler.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
 
     table.put_item(
         Item={
-    "verification": "20260919160306",
+    "verification": "20260919165809",
             "orderId": order_id,
             "userId": "mock-user",
             "productId": "mock-product",
